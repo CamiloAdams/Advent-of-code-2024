eac048e804b794841a49bb8d4e7d3c037365a5b9
Camilo Adames <camiloadames1108@gmail.com>
2024-12-02 03:12:47 -0500

    Add: Initial implementation for reading meal data from input file

---
+++ template/main.py
@@ -0,0 +1,7 @@
+import sys
+# sys.stdout = open('./input.txt', 'w')
+file = open('./day1/input.txt', 'r')
+meals = file.readlines()
+
+# for index, line in enumerate(meals):
+#     print("Line {}: {}".format(index, line.strip()))
